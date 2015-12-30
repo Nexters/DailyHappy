bd5a532522aef38844a6176192f5e04813d06ffe
seokjoong <happychrono@gmail.com>
2015-12-30 19:34:19 +0900

    [Main]  테이블 뷰 커스텀 클래스들 생성 및 연결, 카드 배경색 노란색으로 임시 저장, 플로팅 버튼 글 작성 화면으로 연결

---
+++ DailyHappy/MainItemView.swift
@@ -0,0 +1,43 @@
+//
+//  MainItemView.swift
+//  DailyHappy
+//
+//  Created by seokjoong on 2015. 12. 30..
+//  Copyright © 2015년 TeamNexters. All rights reserved.
+//
+
+import UIKit
+
+class MainItemView: UIView {
+    
+    override init (frame : CGRect) {
+        super.init(frame : frame)
+        addBehavior()
+    }
+    
+    convenience init () {
+        self.init(frame:CGRect.zero)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        addBehavior()
+        //fatalError("This class does not support NSCoding")
+    }
+    
+    func addBehavior (){
+        self.layer.masksToBounds = true
+        self.layer.cornerRadius = 10
+        self.backgroundColor = UIColor.yellowColor()
+    }
+
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ DailyHappy/MainTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainTableViewCell.swift
+//  DailyHappy
+//
+//  Created by seokjoong on 2015. 12. 30..
+//  Copyright © 2015년 TeamNexters. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
