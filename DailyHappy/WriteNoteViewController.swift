2198d454fa12add95c9359b45c30349a9742c609
Skyler.Shin <munkyu.s.shin@gmail.com>
2015-12-31 18:19:35 +0900

    키워드 감정 아이콘 변경

--- DailyHappy/WriteNoteViewController.swift
+++ DailyHappy/WriteNoteViewController.swift
@@ -16,6 +16,13 @@ class WriteNoteViewController: UIViewController{
     @IBOutlet weak var emotionCollectionView: UICollectionView!
     @IBOutlet weak var doneButton: UIButton!
     @IBOutlet weak var keywordTextField: UITextField!
+    @IBOutlet weak var memoTextField: UITextField!
+    @IBOutlet weak var activityButton: UIButton!
+    @IBOutlet weak var itemButton: UIButton!
+    @IBOutlet weak var anniversaryButton: UIButton!
+   
+    @IBOutlet weak var personButton: UIButton!
+    @IBOutlet weak var placeButton: UIButton!
     
     let realm = try! Realm()
     let disposeBag = DisposeBag()
@@ -33,6 +40,8 @@ class WriteNoteViewController: UIViewController{
         
         keywordTextField.attributedPlaceholder = NSAttributedString(string:keywordTextField.placeholder!,attributes: [NSForegroundColorAttributeName: Constants.Color.lightGray])
         
+        memoTextField.attributedPlaceholder = NSAttributedString(string:memoTextField.placeholder!,attributes: [NSForegroundColorAttributeName: Constants.Color.lightGray])
+        
         let subscription = keywordTextField.rx_text
             .subscribeNext{ keyword in
                 switch self.currentKeyword {
@@ -47,7 +56,6 @@ class WriteNoteViewController: UIViewController{
                 case .Place:
                     self.note.placeName = keyword
                 }
-            
         }
         disposeBag.addDisposable(subscription)
     }
@@ -56,30 +64,61 @@ class WriteNoteViewController: UIViewController{
         note.hasActivity = true
         currentKeyword = Constants.Keyword.Activity
         keywordTextField.text = self.note.activityName
+        setKeywordButtonsAlpha()
     }
     
     @IBAction func clickItem(sender: UIButton) {
         note.hasItem = true
         currentKeyword = Constants.Keyword.Item
         keywordTextField.text = self.note.itemName
+        setKeywordButtonsAlpha()
     }
     
     @IBAction func clickAnniversary(sender: UIButton) {
         note.hasAnniversary = true
         currentKeyword = Constants.Keyword.Anniversary
         keywordTextField.text = self.note.anniversaryName
+        setKeywordButtonsAlpha()
     }
     
     @IBAction func clickPerson(sender: UIButton) {
         note.hasPerson = true
         currentKeyword = Constants.Keyword.Person
         keywordTextField.text = self.note.personName
+        setKeywordButtonsAlpha()
     }
     
     @IBAction func clickPlace(sender: UIButton) {
         note.hasPlace = true
         currentKeyword = Constants.Keyword.Place
         keywordTextField.text = self.note.placeName
+        setKeywordButtonsAlpha()
+    }
+    
+    
+    func setKeywordButtonsAlpha() {
+        initializeKeywordButtonsAlpha()
+        
+        switch self.currentKeyword {
+        case Constants.Keyword.Activity:
+            activityButton.alpha = CGFloat(1.0)
+        case Constants.Keyword.Item:
+            itemButton.alpha = CGFloat(1.0)
+        case Constants.Keyword.Anniversary:
+            anniversaryButton.alpha = CGFloat(1.0)
+        case Constants.Keyword.Person:
+            personButton.alpha = CGFloat(1.0)
+        case Constants.Keyword.Place:
+            placeButton.alpha = CGFloat(1.0)
+        }
+    }
+    
+    func initializeKeywordButtonsAlpha() {
+        activityButton.alpha = CGFloat(0.5)
+        itemButton.alpha = CGFloat(0.5)
+        anniversaryButton.alpha = CGFloat(0.5)
+        personButton.alpha = CGFloat(0.5)
+        placeButton.alpha = CGFloat(0.5)
     }
     
 }
@@ -107,6 +146,7 @@ extension WriteNoteViewController: UICollectionViewDelegate {
         doneButton.backgroundColor = getEmotionColor(emotion)
     }
 }
+
 
 func getEmotionColor(emotion: Emotion) -> UIColor {
     let red = CGFloat(emotion.emotionColorRed)
